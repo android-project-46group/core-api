ad1578038568ea0973085354b2c25689a1560b81
kokoichi206 <tt.300607@gmail.com>
2023-02-19 06:10:19 +0000

    feat: ListMembers を実装 (#1)

--- repository/database/database.go
+++ repository/database/database.go
@@ -7,25 +7,29 @@ import (
 
 	"github.com/android-project-46group/core-api/config"
 	"github.com/android-project-46group/core-api/repository"
-	_ "github.com/lib/pq"
+	"github.com/android-project-46group/core-api/util/logger"
+	_ "github.com/lib/pq" // postgres driver
 )
 
 type database struct {
-	db *sql.DB
+	db     *sql.DB
+	logger logger.Logger
 }
 
-func New(ctx context.Context, c config.Config) (repository.Database, error) {
+func New(ctx context.Context, cfg config.Config, logger logger.Logger) (repository.Database, error) {
 	source := fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		c.DBHost, c.DBPort, c.DBUser, c.DBPassword, c.DBName,
+		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName,
 	)
-	sqlDB, err := sql.Open(c.DBDriver, source)
+
+	sqlDB, err := sql.Open(cfg.DBDriver, source)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open sql: %w", err)
 	}
 
 	db := &database{
-		db: sqlDB,
+		db:     sqlDB,
+		logger: logger,
 	}
 
 	return db, nil

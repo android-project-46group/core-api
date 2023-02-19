11a87d56d94a82cce3f0d0392209005f4af69a1f
kokoichi206 <tt.300607@gmail.com>
2023-02-19 09:15:00 +0000

    feat: handler の実装 (#1)

--- handler/download.go
+++ handler/download.go
@@ -1,8 +1,17 @@
 package handler
 
 import (
+	"errors"
+	"fmt"
+	"io"
+
 	pb "github.com/android-project-46group/protobuf/gen/go/protobuf"
 	"github.com/opentracing/opentracing-go"
+)
+
+const (
+	// 一度の stream.Send で送信する容量。
+	streamDataCapacity = 100 * 1024 * 1024 // 100 MB
 )
 
 func (h *handler) DownloadMembersZip(
@@ -15,6 +24,31 @@ func (h *handler) DownloadMembersZip(
 	defer span.Finish()
 
 	h.logger.Infof(ctx, "handler.DownloadMembersZip: group ", req.Group.String())
+
+	reader, err := h.usecase.DownloadMembersZip(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to DownloadMembersZip: %w", err)
+	}
+
+	buf := make([]byte, streamDataCapacity)
+
+	for {
+		size, err := reader.Read(buf)
+		if errors.Is(err, io.EOF) {
+			break
+		}
+
+		if err != nil {
+			return fmt.Errorf("failed to read: %w", err)
+		}
+
+		err = stream.Send(&pb.DownloadMembersZipReply{
+			Data: buf[:size],
+		})
+		if err != nil {
+			return fmt.Errorf("failed to send: %w", err)
+		}
+	}
 
 	return nil
 }
